288f7895903a8c4fe016afee36092703cd682b46
Limon1980 <tarkalmob@yandex.ru>
2020-08-07 22:31:40 +1000

    Дополнительное второе задание выполнено

--- scripts/script.js
+++ scripts/script.js
@@ -6,6 +6,7 @@ let sum = 1;
 for (let i = 0; i < str.length; i++) {
 
   sum = sum * +str[i];
+  // условие когда мы дошли до последнего символа числа
   if (i === str.length - 1) {
     console.log('Произведение чисел по порядку от числа ' + num + ' будет ' + sum);
     let exp = sum ** 3;
