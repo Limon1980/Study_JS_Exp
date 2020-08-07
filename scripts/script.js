fc3868668c5c0ca263436eb70d600cfe5355cc05
Limon1980 <tarkalmob@yandex.ru>
2020-08-07 21:59:12 +1000

    --help

---
+++ scripts/script.js
@@ -0,0 +1,16 @@
+let num = 266219;
+let str = num.toString();
+let sum = 1;
+
+// console.log(str.length);
+for (let i = 0; i < str.length; i++) {
+
+  sum = sum * +str[i];
+  if (i === str.length - 1) {
+    console.log('Произведение чисел по порядку от числа ' + num + ' будет ' + sum);
+    let exp = sum ** 3;
+    console.log('Возведем ' + sum + ' в степень 3 = ' + exp);
+    console.log('Выведем первые две цифры полученного числа ' + exp.toString().slice(0, 2));
+  }
+
+}
