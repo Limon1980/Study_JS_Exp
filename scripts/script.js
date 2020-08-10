e45ea2c1c2fbe1bd857a3b42d1222297ef1835a2
Limon1980 <tarkalmob@yandex.ru>
2020-08-10 22:37:39 +1000

    Задание lang выполнено

--- scripts/script.js
+++ scripts/script.js
@@ -1,1 +1,30 @@
 "use strict";
+
+let lang = "ru";
+let day;
+
+if (lang === "ru") {
+  day = "Пн, Вт, Ср, Чт, Пт, Сб, Вс";
+} else if (lang === "en") {
+  day = "Monday, Tuesday, Wednesday, Thursday, Friday, Saturday, Sunday";
+}
+
+console.log(day.split(","));
+
+switch (day) {
+  case "ru":
+    day = "Пн, Вт, Ср, Чт, Пт, Сб, Вс";
+    break;
+  case "en":
+    day = "Monday, Tuesday, Wednesday, Thursday, Friday, Saturday, Sunday";
+    break;
+}
+
+console.log(day.split(","));
+let langArr = [];
+
+langArr["ru"] = ["Пн", " Вт", " Ср", " Чт", " Пт", " Сб", " Вс"];
+
+langArr["en"] = ["Monday", " Tuesday", " Wednesday", " Thursday", " Friday", " Saturday", " Sunday"];
+
+console.log(langArr[lang]);
