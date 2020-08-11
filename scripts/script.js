abd545e2690651bdc2a0bb6b541fa819569b2325
Limon1980 <tarkalmob@yandex.ru>
2020-08-11 17:16:13 +1000

    Доп. задание к 4 уроку выполнено

--- scripts/script.js
+++ scripts/script.js
@@ -1,1 +1,17 @@
 'use strict';
+
+function strEnter(arg) {
+  if (typeof arg !== 'string') {
+    return 'Это не строка!';
+  } else if (arg.trim().length > 30) {
+    return arg.trim().substring(0, 29) + '...';
+  } else {
+    return arg.trim();
+  }
+}
+
+console.log(
+  strEnter(
+    '  Травка зеленеет, солнышко блестит ласточка с весною в гости к нам летит'
+  )
+);
