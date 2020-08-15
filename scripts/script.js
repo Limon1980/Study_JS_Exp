73e179e46833a78d58b1e348a4acd8b1b48284ea
Limon1980 <tarkalmob@yandex.ru>
2020-08-15 15:41:40 +1000

    Изменил настройки jshintrc "expr": true, переделал решение простые числа

--- scripts/script.js
+++ scripts/script.js
@@ -16,6 +16,24 @@ for (let i = 0; i < 7; i++) {
   }
 }
 
+let part1 = () => {
+  let arr = [
+    '24542',
+    '56484645',
+    '421021168',
+    '8484616',
+    '254151568',
+    '101324864',
+    '48465154',
+  ];
+
+  arr.forEach((item) => {
+    item[0] === '2' || item[0] === '4' ? console.log(item) : {};
+  });
+};
+
+part1();
+
 outer: for (let i = 2; i <= 100; i++) {
   for (let j = 2; j < i; j++) {
     // проверить, делится ли число..
@@ -24,5 +42,31 @@ outer: for (let i = 2; i <= 100; i++) {
     }
   }
 
-  console.log(i + ' Делители этого числа 1 и ' + i);
+  //console.log(i + ' Делители этого числа 1 и ' + i);
 }
+
+// part 2
+
+let primeList = [];
+
+let prime = (number) => {
+  for (let k = 2; k < number; k++) {
+    if (number % k === 0) {
+      return false;
+    }
+  }
+  return true;
+};
+
+let part2 = () => {
+
+  for (let j = 2; j < 100; j++) {
+    if (prime(j)) {
+      primeList.push(j);
+      console.log(`Делители числа ${j}: 1, ${j}`);
+    }
+  }
+  console.log(`Все простые числа: ${primeList}`);
+};
+
+part2();
