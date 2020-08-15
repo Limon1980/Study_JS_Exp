2625c1288c70e0287fad86371b8b5b90c5d77b86
Limon1980 <tarkalmob@yandex.ru>
2020-08-15 16:40:15 +1000

    Выполнил доп. задание к 7 уроку, дни недели цикл

--- scripts/script.js
+++ scripts/script.js
@@ -1,1 +1,26 @@
 'use strict';
+
+
+const arr = ['Понедельник', 'Вторник', 'Среда', 'Четверг', 'Пятница', 'Суббота', 'Воскресенье'];
+const days = [
+  'Воскресенье',
+  'Понедельник',
+  'Вторник',
+  'Среда',
+  'Четверг',
+  'Пятница',
+  'Суббота'
+];
+
+const day = new Date().getDay();
+console.log(days[day]);
+
+arr.forEach((item, index, array) => {
+  if ((item === 'Суббота' || item === 'Воскресенье') && (item !== days[day])) {
+    document.write(`<p><i>${item}</i></p>`);
+  } else if (item === days[day]) {
+    document.write(`<p><b>${item}</b></p>`);
+  } else {
+    document.write(`<p>${item}</p>`);
+  }
+});
