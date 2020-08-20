f728f01a2d79959305e816b084d602f8e86e68f6
Limon1980 <tarkalmob@yandex.ru>
2020-08-20 16:15:47 +1000

    Доп. задание к 10 уроку выполнил

--- scripts/script.js
+++ scripts/script.js
@@ -1,3 +1,21 @@
 'use strict';
 
 
+const inp = document.querySelector('input');
+
+
+let inpText = inp.oninput = function(){
+ return inp.value;
+};
+
+const but = document.querySelector('button');
+
+but.addEventListener('click', function(){
+  let liAll = document.querySelectorAll('li');
+  let len = liAll.length;
+  console.log(liAll);
+  liAll[len-1].insertAdjacentHTML('afterend', `<li>${inpText()[0].toUpperCase() + inpText().substr(1)}</li>`);
+  console.log('Номер');
+});
+
+
